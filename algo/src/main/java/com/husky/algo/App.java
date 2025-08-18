00d2848b524ff18a186b9f24ebeb322fd8cd4a95
li_dingxin <849627763@qq.com>
2025-08-18 13:56:09 +0800

    初始化项目

---
+++ algo/src/main/java/com/husky/algo/App.java
@@ -0,0 +1,13 @@
+package com.husky.algo;
+
+/**
+ * Hello Algo!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Algo!" );
+    }
+}
